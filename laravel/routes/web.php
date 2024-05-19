8a0f2d0872f55ddc99c882be2861c36635bd5cd9
Mefozu <dimabojko074@gmail.com>
2024-05-19 19:37:40 +0300

    авторизация и регистрация

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//
+//Route::get('/',function (){
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function (){
+   return view('auth.autorization');
+});
+
+Route::namespace('Auth')->group(function () {
+    Route::post('/auth', 'AuthController@auth');
 });
